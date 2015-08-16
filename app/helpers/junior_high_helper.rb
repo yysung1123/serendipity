8987ccde0fdcc26c39ab76498b66bf4e37988aec
deror1869107 <sungyuanyao@gmail.com>
2015-08-16 22:15:57 +0800

    Add junior_high controller

---
+++ app/helpers/junior_high_helper.rb
@@ -0,0 +1,2 @@
+module JuniorHighHelper
+end
---
+++ test/controllers/junior_high_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class JuniorHighControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

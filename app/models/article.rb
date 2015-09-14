0401aec5cfe0446b87b84597d9bf73ff2e325e63
Yishen Chang <samkami@icloud.com>
2015-09-14 21:03:10 +0800

    Add comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   belongs_to :topic
+  has_many :comments, dependent: :destory
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901044217) do
+ActiveRecord::Schema.define(version: 20150914124921) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "author"
     t.string   "title"
     t.text     "content"
     t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user"
+    t.string   "content"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

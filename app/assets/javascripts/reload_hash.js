318cff363d471d1284ef02d3c787c634405f3883
deror1869107 <sungyuanyao@gmail.com>
2015-10-23 23:13:12 +0800

    Revert "Merge pull request #4 from deror1869107/slidershow"
    This reverts commit 0d66e9930d91118c2e5bf6cbeeef0eb2fe92aaf6, reversing
    changes made to ce227766c66a621f2a23ebcd37729977f2d34172.

--- app/assets/javascripts/reload_hash.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-        $('a[data-reload="true"').on('click', function(e) {
-            window.location = $(e.target).attr('href');
-            window.location.reload(true);
-        });
-    });

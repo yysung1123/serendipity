b2a08330d29117e098e79ecb66a2af01b6f2a236
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-02-10 15:39:20 +0800

    Article Show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,11 @@
 class CommentsController < ApplicationController
 
-  def new
-    @comment = @article.comments.new
-  end
-
   def create
     @comment = current_user.comments.build(comment_params)
+    @article = @comment.article
     if @comment.save
-     redirect_to root_path
-   end
+     redirect_to topic_article_path(@article.topic_id, @article.id)
+    end
   end
 
   private
